cb6c063355e52ad8f700029591611e9a6873b5bd
manu@DESKTOP-FP8RVCH. <manu@DESKTOP-FP8RVCH.localdomain>
2025-07-22 11:02:33 -0300

    main y .h posibles

---
+++ SOPA_DE_LETRAS.C
@@ -0,0 +1,15 @@
+#include "SOPA_DE_LETRAS.H"
+#include "VECTOR_DINAMICO.H"
+
+
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+void generar_terreno(){
+
+}
+
+char* pedir_palabra(){
+    
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main(char* argv[], int* argc){
+    
+
+
+return 0;
+}
